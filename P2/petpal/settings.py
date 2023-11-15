79749cfaf11e5040275178907dc7ccf67e76c5a6
Nafio Miah <nafiomiah@hotmail.com>
2023-11-14 19:58:47 -0500

    Add missing comma in settings.py installed apps

--- P2/petpal/settings.py
+++ P2/petpal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'petListing',
-    'django_filters'
+    'django_filters',
     'accounts',
     'rest_framework',
     'rest_framework_simplejwt',
