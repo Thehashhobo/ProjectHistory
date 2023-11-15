dafebd4204c111e47d42da5cb08b31c84a73f704
abdal129 <markus@markus.com>
2023-11-15 14:59:12 -0500

    Update files

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,6 +1,6 @@
 python3 -m pip install virtualenv
 python3 -m virtualenv venv
-python3 ../venv/Scripts/activate
+source ./venv/Scripts/activate
 pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
