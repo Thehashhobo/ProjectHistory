6b81d8fa5b83f47ccde3f869055b25e2032516a5
sms-sudo <irock12022@gmail.com>
2023-11-30 19:19:08 -0500

    client side file setup

---
+++ P3/client/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ChakraProvider } from '@chakra-ui/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  Home,
+  Search,
+  SavedProjects,
+  Login,
+  SignUp,
+  CreateProject,
+} from './pages';
+import Layout from './components/Layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/search',
+        element: <Search />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
