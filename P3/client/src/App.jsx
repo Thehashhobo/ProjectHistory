210eee658780f70f404a0ad25a4539f94f39595f
Jerry Wang <79975526+thehoodhobo@users.noreply.github.com>
2023-12-12 00:59:14 -0500

    fix client

---
+++ P3/client/src/App.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ChakraProvider } from '@chakra-ui/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import PetListingPage from './pages/petListing/PetListingPage.jsx';
+import PetListingDetailPage from './pages/petListing/PetListingDetailPage.jsx';
+import { Home } from './pages';
+import Layout from './components/Layout';
+import RegistrationPage from './pages/accounts/Register.jsx';
+import LoginPage from './pages/accounts/Login.jsx';
+import ListingPetSheltersPage from './pages/accounts/ListingShelters.jsx';
+import ShelterDetailPage from './pages/accounts/DetailShelter.jsx';
+import AccountInfoPage from './pages/accounts/AccountInfoPage.jsx';
+import PetPalUserUpdatePage from './pages/accounts/PetPalUserUpdatePage.jsx';
+import PetSeekerDetailPage from './pages/accounts/DetailSeeker.jsx';
+import ConversationPage from './pages/comments/ConversationPage.jsx';
+import PageNotFound from './components/PageNotFound.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/pet-listings', // path to Petlisting List
+        element: <PetListingPage />,
+      },
+      {
+        path: '/pet-details/:petId', // path to Pet detail List
+        element: <PetListingDetailPage />,
+      },
+      {
+        path: '/register',
+        element: <RegistrationPage />,
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/pet_shelters',
+        element: <ListingPetSheltersPage />,
+      },
+      {
+        path: '/pet_shelters/:petShelterID',
+        element: <ShelterDetailPage />,
+      },
+      {
+        path: '/pet-details/application/:petId', // path to Pet detail List
+        element: <Home />, // change element to your application page(if you are using page,but i think pop up might be better)
+        //and use const { petId } = useParams(); to grab petId
+      },
+      {
+        path: '/account-information',
+        element: <AccountInfoPage />,
+      },
+      {
+        path: '/account-update',
+        element: <PetPalUserUpdatePage />,
+      },
+      {
+        path: '/pet_seeker/:petSeekerID',
+        element: <PetSeekerDetailPage />,
+      },
+      {
+        path: '/application/:appId',
+        element: <ConversationPage />,
+      },
+      {
+        path: '/*',
+        element: <PageNotFound />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
---
+++ P3/client/src/components/petListings/CreateUpdateListing.jsx
@@ -0,0 +1,269 @@
+import axios from 'axios';
+import React, { forwardRef, useImperativeHandle } from 'react';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from "@hookform/error-message"
+
+
+const PetListingForm = forwardRef(({ onFormSubmitSuccess, predefinedValues }, ref) => {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: predefinedValues || {
+            name: '',
+            breed: '',
+            age: '',
+            size: '',
+            color: '',
+            gender: '',
+            description: '',
+            characteristics: '',
+            avatar: null // Assuming avatar is a file input
+        }
+    });
+    const isUpdate = () =>{
+        // Check if predefinedValues is not null and has at least one key-value pair
+        return predefinedValues && Object.keys(predefinedValues).length > 0;
+
+    }
+    
+    const onSubmit = async (data) => {
+        console.log('Form data on submit:', data);
+        const petId = predefinedValues?.petId; // Assuming predefinedValues includes the pet ID
+        let queryString = `http://127.0.0.1:8000/petListing/${isUpdate() ? `${petId}/` : ''}`;
+    
+        const requestData = isUpdate() ? {
+            description: data.description,
+            characteristics: data.characteristics,
+            avatar: data.avatar, // Assuming this is a file or a URL
+            status: data.status
+        } : {
+            name: data.name,
+            breed: data.breed,
+            age: data.age,
+            size: data.size,
+            color: data.color,
+            gender: data.gender,
+            description: data.description,
+            shelter: localStorage.getItem('user_id'),//may cause bug, this is a user_id and not the shelter id
+            date_posted: Date.now(),
+            characteristics: data.characteristics,
+            avatar: data.avatar
+        };
+        console.log('Request data:', requestData);
+
+        try {
+            const accessToken = localStorage.getItem('access_token');
+            const method = isUpdate() ? axios.put : axios.post;
+    
+            await method(queryString, requestData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `Bearer ${accessToken}`
+                },
+            });
+    
+            // If successful:
+            if (onFormSubmitSuccess) {
+                onFormSubmitSuccess(); // Notify parent component of success
+                console.log('Form submitted successfully');
+            }
+        } catch (error) {
+            console.error(`Error ${isUpdate() ? 'updating' : 'creating'} pet listing:`, error);
+        }
+    };
+    
+      
+
+    // Expose the submit function to parent via `ref`
+    useImperativeHandle(ref, () => ({
+        submitForm: () => handleSubmit(onSubmit)() // Correctly execute the function returned by handleSubmit
+    }));
+    
+
+
+    const formStyle = {
+        display: 'grid', 
+        gridTemplateColumns: 'repeat(2, 1fr)', 
+        gap: '20px',
+    };
+
+    const labelStyle = {
+        fontSize: '1.2em', // Increase the font size of the label
+        marginBottom: '5px', // Spacing between the label and input field
+    };
+
+    const getInputBorder = (isDisabled, oppsite) => ({
+        padding: '10px',
+        border: '2px solid #ccc',
+        borderRadius: '4px',
+        backgroundColor: (isDisabled  && oppsite)? '#f0f0f0' : 'white' // Grey background if disabled
+    });
+
+    const groupStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+    }
+
+    const redText = {
+        color: "#de1818"
+    }
+
+
+    return (
+      <form onSubmit={handleSubmit(onSubmit)} style={formStyle}>
+        <div style={groupStyle}>
+          <label htmlFor="name" style={labelStyle}>
+            Pet Name
+          </label>
+          <input
+            id="name"
+            style={getInputBorder(isUpdate(), true)}
+            {...register("name", { required: true })}
+            disabled={isUpdate()}
+          />
+          {errors.name && <span style={redText}>This field is required</span>}
+        </div>
+
+        <div style={groupStyle}>
+          <label htmlFor="breed" style={labelStyle}>
+            Breed
+          </label>
+          <input
+            id="breed"
+            style={getInputBorder(isUpdate(), true)}
+            {...register("breed", { required: true })}
+            disabled={isUpdate()}
+          />
+          {errors.breed && <span style={redText}> This field is required</span>}
+        </div>
+
+        <div style={groupStyle}>
+          <label htmlFor="age" style={labelStyle}>
+            Age
+          </label>
+          <input
+            id="age"
+            style={getInputBorder(isUpdate(), true)}
+            {...register("age", {
+              required: "This field is required",
+              pattern: {
+                value: /^[0-9]+$/, // Regex for positive numbers only
+                message: "Please input a positive number",
+              },
+            })}
+            disabled={isUpdate()}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="age" // Match the field name
+            render={({ message }) => <p style={redText}>{message}</p>}
+          />
+        </div>
+
+        <div style={groupStyle}>
+          <label htmlFor="size" style={labelStyle}>
+            Size
+          </label>
+          <select
+            id="size"
+            style={getInputBorder(isUpdate(), true)}
+            {...register("size", { required: true })}
+            disabled={isUpdate()}
+          >
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+            <option value="extra_large">Extra Large</option>
+          </select>
+          {errors.size && <span style={redText}>This field is required</span>}
+        </div>
+
+        <div style={groupStyle}>
+          <label htmlFor="color" style={labelStyle}>
+            Color
+          </label>
+          <input
+            id="color"
+            style={getInputBorder(isUpdate(), true)}
+            {...register("color", { required: true })}
+            disabled={isUpdate()}
+          />
+          {errors.color && <span style={redText}>This field is required</span>}
+        </div>
+
+        <div style={groupStyle}>
+          <label htmlFor="gender" style={labelStyle}>
+            Gender
+          </label>
+          <select
+            id="gender"
+            style={getInputBorder(isUpdate(), true)}
+            {...register("gender", { required: true })}
+            disabled={isUpdate()}
+          >
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+          </select>
+          {errors.gender && <span style={redText}>This field is required</span>}
+        </div>
+
+        <div style={groupStyle}>
+          <label htmlFor="description" style={labelStyle}>
+            Description
+          </label>
+          <textarea
+            id="description"
+            style={getInputBorder(isUpdate(), false)}
+            {...register("description", { required: true })}
+          />
+          {errors.description && (
+            <span style={redText}>This field is required</span>
+          )}
+        </div>
+
+        <div style={groupStyle}>
+          <label htmlFor="characteristics" style={labelStyle}>
+            Characteristics
+          </label>
+          <input
+            id="characteristics"
+            style={getInputBorder(isUpdate(), false)}
+            {...register("characteristics", { required: true })}
+          />
+          {errors.characteristics && (
+            <span style={redText}>This field is required</span>
+          )}
+        </div>
+
+        <div style={groupStyle}>
+          <label htmlFor="avatar" style={labelStyle}>
+            Avatar
+          </label>
+          <input
+            id="avatar"
+            type="file"
+            style={getInputBorder(isUpdate(), false)}
+            {...register("avatar")}
+          />
+        </div>
+
+        <div style={groupStyle}>
+          <label htmlFor="status" style={labelStyle}>
+            Status
+          </label>
+          <select
+            id="status"
+            style={getInputBorder(isUpdate(), false)}
+            {...register("status")}
+            disabled={!isUpdate()}
+          >
+            <option value="available">Available</option>
+            <option value="adopted">Adopted</option>
+            <option value="pending">Pending</option>
+            <option value="unavailable">Unavailable</option>
+          </select>
+        </div>
+      </form>
+    );
+});
+
+
+export default PetListingForm;
